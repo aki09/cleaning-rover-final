dc06c10488ca21b4f78c964afa20b28373a0c02b
akshat <akshatbhatia05@gmail.com>
2022-11-27 00:10:36 +0530

    clean

--- src/start.py
+++ src/start.py
@@ -6,13 +6,16 @@ import src.Mongo as Mongo
 def initRoverOnMongo(rover,dataCollection):
     Mongo.mongoConnectRoverBySerial(rover=rover,dataCollection=dataCollection)
 
+def cleanArea():
+    print('cleaning')
+
 def mainStart(serial=None, connection=None,dataCollection=None):
     if serial != None:
         print(serial)
         rover = Rover(roverSerial=serial,connection=connection)
         print(rover)
         initRoverOnMongo(rover=rover,dataCollection=dataCollection)
-
+        cleanArea()
 
 if __name__ == '__main__':
     pass
